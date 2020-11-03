28c2db7d34b507afc215485341808087067f4e89
Jamman72341 <52226470+Jamman72341@users.noreply.github.com>
2020-11-04 09:33:22 +1300

    Add files via upload

---
+++ theme/bottombit.php
@@ -0,0 +1,7 @@
+<div class="footer">
+		&copy; Miss Gottschalk 2015
+	</div> <!-- end footer -->
+
+</div> <!-- end content -->
+
+</body>
---
+++ theme/heading.php
@@ -0,0 +1,3 @@
+<div class="heading">
+		<h1 class="white">Chic Clothing</h1>
+	</div> <!-- end heading -->
